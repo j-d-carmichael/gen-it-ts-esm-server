db54b07133604859c356a98864ddca11031e71ed
John Carmichael <john.carmichael@extern.acrontum.co.za>
2025-03-04 09:42:12 +0000

    fix: ignore tests in the exclude folder .openapi-nodegen

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     include: ['**/*.test.ts'],
-    exclude: ['node_modules', 'dist'],
+    exclude: ['node_modules', 'dist', '.openapi-nodegen'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
